7420d06ff7eaa1c68caf1777cc77490c15d74049
sudsymoss <james1mowat@gmail.com>
2024-11-20 10:07:27 +1300

    update data

--- auto-refresh.go
+++ auto-refresh.go
@@ -9,35 +9,30 @@ import (
 func (v Database) EnableAutoUpdateGTFSData() {
 	c := cron.New()
 
-	// Add the cron schedule and task to the cron job
-	c.AddFunc("@every 00h30m00s", func() {
-		isValid, err := v.IsFeedDataUpToDate()
-		if err != nil {
-			fmt.Println(err)
-		}
-		if !isValid {
-			fmt.Println("Feed data is outdated, updating...")
-
-			// Delete old data if it's outdated
-			err := v.deleteOldData()
-			if err != nil {
-				fmt.Printf("Failed to delete old data: %v", err)
-			}
-
-			// Fetch and write new data
-			data, err := fetchZip(v.url)
-			if err != nil {
-				fmt.Printf("Failed to fetch new data: %v", err)
-			}
-			err = writeFilesToDB(data, v.db)
-			if err != nil {
-				fmt.Printf("Failed to write new data to the database: %v", err)
-			}
-			fmt.Println("Feed data updated automatically")
-		}
-
+	c.AddFunc("@every 01h00m00s", func() {
+		updateData(v)
 	})
 
 	// Start the cron job scheduler
 	c.Start()
+}
+
+func updateData(v Database) {
+	fmt.Println("Feed data is outdated, updating...")
+
+	// Delete old data if it's outdated
+	err := v.deleteOldData()
+	if err != nil {
+		fmt.Printf("Failed to delete old data: %v", err)
+	}
+
+	// Fetch and write new data
+	data, err := fetchZip(v.url)
+	if err != nil {
+		fmt.Printf("Failed to fetch new data: %v", err)
+	}
+	err = writeFilesToDB(data, v.db)
+	if err != nil {
+		fmt.Printf("Failed to write new data to the database: %v", err)
+	}
 }
